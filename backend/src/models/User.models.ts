88efbf4fd3d43edd1c61a6c96cbb505bdd316cc6
RotemNissim <rotemnissim28@gmail.com>
2025-02-22 15:39:33 +0200

    minor fixes in interface export, adding logout route.

--- backend/src/models/User.models.ts
+++ backend/src/models/User.models.ts
@@ -8,14 +8,16 @@ export interface IUser extends Document {
   calendar: any[];
   profilePicture: string;
 }
-
-const UserSchema: Schema = new Schema({
+const UserSchema = new mongoose.Schema<IUser>({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   hobbies: [{ type: String }],
   calendar: [{ type: Object }],
-  profilePicture: { type: String, default: '' }
-}, { timestamps: true });
+  profilePicture: { type: String, default: '' },
+});
 
-export default mongoose.model<IUser>('User', UserSchema);
+
+const userModel = mongoose.model<IUser>('User', UserSchema);
+
+export default userModel;
