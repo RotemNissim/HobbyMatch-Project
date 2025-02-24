0408d81d67863ded8e9ec2cdbb15ab7440e91983
RotemNissim <rotemnissim28@gmail.com>
2025-02-24 12:29:41 +0200

    fixing app.ts, server.ts and admin routes
    overload on router.post (admin route) fixed
    logic of app & server changed
    server is running as expected.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,47 +1,12 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
-import cors from 'cors';
+import initApp from "./server";
 import dotenv from 'dotenv';
-import { authenticateToken } from './middleware/authMiddleware';
-
-// Import Routes
-import authRoutes from './routes/auth.routes';
-import userRoutes from './routes/user.routes';
-import eventRoutes from './routes/event.routes';
-import hobbyRoutes from './routes/hobby.routes';
-import likeRoutes from './routes/like.routes';
-import messageRoutes from './routes/message.routes';
-import notificationRoutes from './routes/notification.routes';
-
 dotenv.config();
+const port = process.env.PORT || 3000;
 
-const app: Application = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', authenticateToken, userRoutes);
-app.use('/api/events', eventRoutes);
-app.use('/api/hobbies', hobbyRoutes);
-app.use('/api/likes', likeRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/notifications', notificationRoutes);
-
-// Root Route
-app.get('/', (req: Request, res: Response) => {
-  res.send('API is running...');
+console.log("1");
+initApp().then((app) => {
+  app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`);
 });
-
-// Global Error Handler
-app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof Error) {
-    console.error(err.message);
-    res.status(500).json({ error: err.message });
-  } else {
-    res.status(500).json({ error: 'An unknown error occurred' });
-  }
 });
-
-export default app;
