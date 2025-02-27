7c1c207293d5333793ac9eace059dd8b1bef2811
galMiron5 <136713466+galMiron5@users.noreply.github.com>
2025-02-27 17:47:20 +0200

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,10 +4,10 @@ import initApp from "./server";
 
 const port = process.env.PORT || 3000;
 
-
 console.log("1");
 initApp().then((app) => {
   app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
+  });
 });
-});
+console.log("Gal is the King");
