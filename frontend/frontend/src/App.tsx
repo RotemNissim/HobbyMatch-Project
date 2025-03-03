f7bf0d6a8cc1697ad5a8260bb6a99ab9b2106d88
galMiron5 <136713466+galMiron5@users.noreply.github.com>
2025-03-03 17:09:43 +0200

    styles

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,8 +1,10 @@
+
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './router/AppRouter';
 import Layout from './components/layout';
 
+import "./styles/global.css";
 const App: React.FC = () => (
     <BrowserRouter>
     <Layout>
