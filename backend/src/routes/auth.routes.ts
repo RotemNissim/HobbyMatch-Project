1a5c9c2f40e314b02d894bf7abe1ff90d53ebaa3
galMiron5 <136713466+galMiron5@users.noreply.github.com>
2025-03-15 16:21:58 +0200

    Update auth.routes.ts

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,20 +1,200 @@
 import express from "express";
 const router = express.Router();
 import authController from "../controllers/auth.controller";
+
 /**
  * @swagger
  * tags:
  *   name: Auth
  *   description: The Authentication API
  */
-router.post("/google", authController.googleSignIn);
 
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *         - firstName
+ *         - lastName
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: The user email
+ *         password:
+ *           type: string
+ *           description: The user password
+ *         firstName:
+ *           type: string
+ *           description: The user first name
+ *         lastName:
+ *           type: string
+ *           description: The user last name
+ *       example:
+ *         email: 'bob@gmail.com'
+ *         password: '123456'
+ *         firstName: 'bob'
+ *         lastName: 'marly'
+ */
+
+ 
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The user email
+ *               password:
+ *                 type: string
+ *                 description: The user password
+ *             example:
+ *               email: 'bob@gmail.com'
+ *               password: '123456'
+ *     responses:
+ *       200:
+ *         description: The authenticated user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *                 refreshToken:
+ *                   type: string
+ *                 _id:
+ *                   type: string
+ *                 role:
+ *                   type: string
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.post("/login", authController.login);
 
+/**
+ * @swagger
+ * /auth/refresh:
+ *   post:
+ *     summary: Refresh access token
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: New access token issued
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *                 refreshToken:
+ *                   type: string
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.post("/refresh", authController.refresh);
 
+/**
+ * @swagger
+ * /auth/logout:
+ *   post:
+ *     summary: Log out the user
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully logged out
+ *       500:
+ *         description: Internal server error
+ */
 router.post("/logout", authController.logout);
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Registers a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       201:
+ *         description: The new user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     firstName:
+ *                       type: string
+ *                     lastName:
+ *                       type: string
+ *                     email:
+ *                       type: string
+ *                     refreshToken:
+ *                       type: string
+ *                     password:
+ *                       type: string
+ *                     hobbies:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *                     calendar:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *                     profilePicture:
+ *                       type: string
+ *                     likes:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *                     _id:
+ *                       type: string
+ *                     __v:
+ *                       type: integer
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 router.post("/register", authController.register);
 
 export default router;
